4e8aabbce10539d8d91aefc08493f1a22707dcf3
Luiz Augusto <augustoaraujoesl@gmail.com>
2023-02-13 15:09:27 -0300

    project jsp

---
+++ src/main/java/org/webapp/HolaMundoServlet.java
@@ -0,0 +1,8 @@
+package org.webapp;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/hola-mundo")
+public class HolaMundoServlet extends HttpServlet {
+}
